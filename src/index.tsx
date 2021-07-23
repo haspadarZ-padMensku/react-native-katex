4337e789946f264f13028ac88b12b87ad390fea3
Denis Goncharov <gonchds@gmail.com>
2021-07-23 17:45:01 +0300

    Add auto height; fix scaling; small improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import katexStyle from './katex-style';
 import katexScript from './katex-script';
 
 export interface TrustContext {
-  command: string
-  url: string
-  protocol: string
+  command: string;
+  url: string;
+  protocol: string;
 }
 
 export interface KatexOptions {
@@ -43,9 +43,12 @@ ${inlineStyle}
 </style>
 <script>
 window.onerror = e => document.write(e);
-window.onload = () => katex.render(${JSON.stringify(expression)}, document.body, ${JSON.stringify(options)});
+window.onload = () => katex.render(${JSON.stringify(
+    expression,
+  )}, document.body, ${JSON.stringify(options)});
 ${katexScript}
 </script>
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
 </body>
@@ -67,27 +70,73 @@ html, body {
   height: 100%;
   margin: 0;
   padding: 0;
+  overflowX: auto;
 }
 .katex {
   margin: 0;
   display: flex;
+  overflowX: auto;
 }
 `;
 
 export interface KatexProps extends ContentOptions {
-  style: StyleProp<ViewStyle>;
+  style: object;
+  containerStyle: object;
   onLoad: any;
   onError: any;
+  injectedJavaScript?: any;
+  onMessage?: any;
 }
 
-export default function Katex({ style, onLoad, onError, ...options }: KatexProps) {
-  return <WebView
-      style={style}
+export default function Katex({
+  style,
+  containerStyle,
+  onLoad,
+  onError,
+  injectedJavaScript,
+  onMessage,
+  ...options
+}: KatexProps) {
+  const [height, setHeight] = useState(0);
+
+  const webViewScript = `
+    setTimeout(function() {
+      const el = document.querySelector(".katex");
+      window.ReactNativeWebView.postMessage(el.scrollHeight); 
+    }, 100);
+    true; // note: this is required, or you'll sometimes get silent failures
+  `;
+
+  const handleMessage = useCallback(
+    (event: any) => {
+      try {
+        const newHeight = parseInt(event.nativeEvent.data);
+        height !== newHeight && setHeight(newHeight);
+      } catch (error) {
+        console.error(error);
+        return;
+      }
+    },
+    [height],
+  );
+
+  return (
+    <WebView
+      style={{ ...style, height }}
+      containerStyle={{ ...containerStyle }}
       source={{ html: getContent(options) }}
       onLoad={onLoad}
       onError={onError}
       renderError={onError}
-  />;
+      injectedJavaScript={injectedJavaScript || webViewScript}
+      onMessage={onMessage || handleMessage}
+      javaScriptEnabled={true}
+      scrollEnabled={false}
+      useWebKit={true}
+      automaticallyAdjustContentInsets={false}
+      scalesPageToFit={false}
+    />
+  );
 }
 
 Katex.defaultProps = {
@@ -97,8 +146,9 @@ Katex.defaultProps = {
   errorColor: '#f00',
   inlineStyle: defaultInlineStyle,
   style: defaultStyle,
+  containerStyle: { flex: 0 },
   macros: {},
   colorIsTextColor: false,
-  onLoad: Boolean,
-  onError: Boolean,
+  onLoad: () => { },
+  onError: (e: any) => console.log(e),
 };
